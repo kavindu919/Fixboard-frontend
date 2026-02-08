1219d108a022f42d7eb936f0d94bf05daea7c153
kavindu919 <jayakodyk2@gmail.com>
2026-02-08 19:42:03 +0530

    feat: add axios instance, tailwind and clean up default vite react files

---
+++ src/api/axiosInstance.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.BASE_URL,
+  timeout: 1000,
+  headers: { "Content-Type": "application/json" },
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token && config.headers) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default axiosInstance;
