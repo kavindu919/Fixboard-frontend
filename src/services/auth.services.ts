ce24cd1f60baa49b9a883d545a0e386bed55e9b3
kavindu919 <jayakodyk2@gmail.com>
2026-02-11 19:29:19 +0530

    feat: create logout for system

--- src/services/auth.services.ts
+++ src/services/auth.services.ts
@@ -1,12 +1,9 @@
-import axiosInstance from "../api/axiosInstance";
-import type {
-  loginPageInterface,
-  registerPageInterface,
-} from "../utils/interfaces/authInterface";
+import axiosInstance from '../api/axiosInstance';
+import type { loginPageInterface, registerPageInterface } from '../utils/interfaces/authInterface';
 
 export const userRegistration = async (validData: registerPageInterface) => {
   try {
-    const res = await axiosInstance.post("/auth/register", validData);
+    const res = await axiosInstance.post('/auth/register', validData);
     return res;
   } catch (error) {
     throw error;
@@ -14,7 +11,16 @@ export const userRegistration = async (validData: registerPageInterface) => {
 };
 export const userLogin = async (validData: loginPageInterface) => {
   try {
-    const res = await axiosInstance.post("/auth/login", validData);
+    const res = await axiosInstance.post('/auth/login', validData);
+    return res;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const userLogout = async () => {
+  try {
+    const res = await axiosInstance.post('/auth/logout');
     return res;
   } catch (error) {
     throw error;
