f59d1fe5a476ef84f496edb884f13c4aeb83a034
kavindu919 <jayakodyk2@gmail.com>
2026-02-11 19:47:31 +0530

    style : remove h over effect from create issue button

--- src/pages/dashboard/issue/AllIssues.tsx
+++ src/pages/dashboard/issue/AllIssues.tsx
@@ -117,8 +117,8 @@ const AllIssues = () => {
           </h5>
         </header>
         <Link
-          to="/issues/create"
-          className="bg-secondary hover:bg-accent/90 focus-visible:ring-accent inline-flex items-center justify-center rounded-md px-4 py-2 text-sm font-medium text-white transition-colors focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none"
+          to="/dashboard/issues/create"
+          className="bg-secondary inline-flex items-center justify-center rounded-md px-4 py-2 text-sm font-medium text-white transition-colors focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none"
         >
           Create Issue
         </Link>
